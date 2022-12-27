71bcd9dea427b568618908af92e04dfcbcd6d18c
Annie Huang <annie.hn@gmail.com>
2022-12-27 21:09:51 +1100

    !!!! Get custom property value defined in the css file  in javascript and increase|decrease value according the classList

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('click', (e) => {
   let handle;
 
-  if (e.target.match('.handle')) {
+  if (e.target.matches('.handle')) {
     handle = e.target;
   } else {
     handle = e.target.closest('.handle');
@@ -13,4 +13,17 @@ document.addEventListener('click', (e) => {
   }
 });
 
-function onHandleClick(handle) {}
+function onHandleClick(handle) {
+  const slider = handle.closest('.container').querySelector('.slider');
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue('--slider-index')
+  );
+
+  if (handle.classList.contains('left-handle')) {
+    slider.style.setProperty('--slider-index', sliderIndex - 1);
+  }
+
+  if (handle.classList.contains('right-handle')) {
+    slider.style.setProperty('--slider-index', sliderIndex + 1);
+  }
+}
